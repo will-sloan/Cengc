197a804d6f6aa63b838b0dbd0830ecf6a6225529
Will Sloan <willsloan@cmail.carleton.ca>
2023-11-19 11:39:23 -0500

    DC motor logic

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,14 @@
 #include <AFMotor.h>
 
 AF_DCMotor motor(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+AF_DCMotor motor2(4, MOTOR12_64KHZ); // create motor #4, 64KHz pwm
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("C-Eng C!");
   
   motor.setSpeed(100);     // set the speed to 200/255
+  motor2.setSpeed(100);
 }
 
 void loop() {
@@ -33,8 +35,11 @@ void loop() {
   motor.run(RELEASE);
 
   // Now we do the wheel.
+  Serial.println("Running wheel.");
+  motor2.run(FORWARD);
+  delay(30000);
 
-  
+
 
   
   
