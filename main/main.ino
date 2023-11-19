a8a894ca3364e9cebe94c2e473240a3d791446bc
Will Sloan <willsloan@cmail.carleton.ca>
2023-11-19 15:39:48 -0500

    final code

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,23 @@
 #include <AFMotor.h>
 
-AF_DCMotor motor(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
-AF_DCMotor motor2(4, MOTOR12_64KHZ); // create motor #4, 64KHz pwm
+AF_DCMotor motor(4, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+AF_DCMotor motor2(3, MOTOR12_1KHZ); // create motor #4, 64KHz pwm
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("C-Eng C!");
   
-  motor.setSpeed(100);     // set the speed to 200/255
-  motor2.setSpeed(100);
+  motor.setSpeed(255);     // set the speed to 200/255
+  motor2.setSpeed(25);
 }
 
 void loop() {
-  // Wait a bit for the thing to be in place
+  // // Wait a bit for the thing to be in place
   delay(5000);
   Serial.println("Lift the bin!");
   motor.run(FORWARD);
   // Wait until its at the top
-  delay(3000);
+  delay(200);
 
   // Wait at the top 
   Serial.println("At top, stop.");
@@ -28,19 +28,21 @@ void loop() {
   // Go back down.
   Serial.println("Going down.");
   motor.run(BACKWARD);
-  delay(3000);
+  delay(250);
 
   // Stop motor.
   Serial.println("Stop Motor.");
   motor.run(RELEASE);
 
   // Now we do the wheel.
-  Serial.println("Running wheel.");
-  motor2.run(FORWARD);
+  // Since this did not work, we commented it out. 
+  // motor2.setSpeed(100);
+  // motor2.run(FORWARD);
+  // delay(1000);
+  // motor2.setSpeed(65);
+  // delay(1000);
+  // Serial.println("Running wheel.");
+  // motor2.run(FORWARD);
   delay(30000);
-
-
-
-  
   
 }
